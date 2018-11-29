409f13f1aee97044d46b73c83de5411f127882e7
Gimenez Robin <gimenez.robin11@gmail.com>
2018-11-29 09:55:50 +0100

    Update Routeur.php and login view added

--- Vue/VueHomeLogin.php
+++ Vue/VueHomeLogin.php
@@ -1,5 +1,7 @@
 ﻿<?php $this->titre = "Home | Login"; ?>
 
 <form method="POST" action="index.php?home=sendLogin">
-  
+    <input type="text" name="idLogin" placeholder="Identifiant">
+    <input type="password" name="passLogin" placeholder="Password">
+    <input type="submit" value="Se Connecter">
 </form>
