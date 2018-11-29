8b24fedc284670df70744703b853661b8b76f0c1
Gimenez Robin <gimenez.robin11@gmail.com>
2018-11-29 11:19:08 +0100

    New gabarit for Home Pages and update vue.php

---
+++ Vue/gabaritHome.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="Contenu/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="Contenu/style.min.css" rel="stylesheet">
+    <title><?=$titre ?></title>
+  </head>
+
+  <body>
+    <?=$contenu ?>
+  </body>
+
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+
+
+
+  <script src="js/main.js"></script>
+</html>
